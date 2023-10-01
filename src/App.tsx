844d1f02f5400c2068102fc96172d7e325536ca4
haikaljs <haikaljavascript@gmail.com>
2023-10-01 11:37:20 +0800

    App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,12 @@
-import React from "react";
-
 import "./App.css";
 
-let bio: any; // not recommended
-let address: unknown; // not recommended
-let name: string; // string
-let age: number; // number
-let isStudent: boolean; // boolean
-let hobbies: string[]; // array of string
-let rating: number | string; // union
-
-let role: [number, string]; // tuple (contain fixed value)
-
-let person1: Object; // not recommeded
-
-type Person = {
-  name: string;
-  age: number;
-};
-
-let person: Person = {
-  name: "Haikal",
-  age: 31,
-};
-
-let lotsOfPeople: Person[]; // Array of object
-
-function printName(name: string) {
-  console.log(name);
-}
-
-printName("haikal");
-
-let printAge: Function; // Function type
-let printBio: (name: string) => void; // Function type better way, void return undefined, never return nothing
-
-// Interface
-interface Man {
-  name: string;
-  age?: number;
-}
-
-type X = {
-  a: string;
-  b: number;
-};
-
-type Y = X & {
-  // extending types
-  // if we want to use X in Y
-  c: string;
-  d: number;
-};
-
-let y: Y = {
-  a: "Haikal",
-  b: 31,
-  c: "along",
-  d: 32,
-};
-
-interface Women extends Person {
-  job: string;
-}
-
-type Z = Women & {
-  isWifi: true;
-};
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Drag n drop</h1>
+      <span className="heading">Task</span>
+
     </div>
   );
-}
+};
 
 export default App;

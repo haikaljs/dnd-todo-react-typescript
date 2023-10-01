b4e7f76718a111e511d2dd664404df049c28d525
haikaljs <haikaljavascript@gmail.com>
2023-10-01 11:54:34 +0800

    InputField component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,25 @@
+import "./style.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form className="input">
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Type our task..."
+        className="input__box"
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;

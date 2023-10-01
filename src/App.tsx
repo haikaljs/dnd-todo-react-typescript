c44dfc0fbf222a8d3649d3adde5079d5a5756cd3
haikaljs <haikaljavascript@gmail.com>
2023-10-02 00:18:21 +0800

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [todo, setTodo] = useState<string>("")
-  console.log(todo)
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
   return (
     <div className="App">
       <span className="heading">Task</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList /> */}
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
